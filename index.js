c8bb3380532519de75a33862833647eb08262dc9
Aliu Yusuf <aliu.yusuf@outlook.com>
2020-07-28 09:40:16 +0100

    Finished Ecmascript intro

--- index.js
+++ index.js
@@ -1,125 +1,125 @@
-// //Template literals
-// let word1 = 'Yusuf';
-// let word2  = 'Aliu';
+//Template literals
+let word1 = 'Yusuf';
+let word2  = 'Aliu';
 
-// let fullName= `${word1}
-// ${word2}
-// `
-// document.getElementById('test').innerText = `${fullName}`
+let fullName= `${word1}
+${word2}
+`
+document.getElementById('test').innerText = `${fullName}`
 
-//Destructuring Objects
-// const personalInformation = {
-//   firstName: 'Yusuf',
-//   lastName: 'Aliu',
-//   email:'yusuf@zyl.com'
-// }
+Destructuring Objects
+const personalInformation = {
+  firstName: 'Yusuf',
+  lastName: 'Aliu',
+  email:'yusuf@zyl.com'
+}
 
-// // const {firstName,lastName} = personalInformation;
-// const {firstName:fn,lastName:ln} = personalInformation;
-// console.log(`My name is ${fn} ${ln}`);
+// const {firstName,lastName} = personalInformation;
+const {firstName:fn,lastName:ln} = personalInformation;
+console.log(`My name is ${fn} ${ln}`);
 
-//Destructuring Arrays
+Destructuring Arrays
 
-// let list = ['Yusuf','FrontEnd','Tavia'];
+let list = ['Yusuf','FrontEnd','Tavia'];
 
-// let [firstName,skill,company] = ['Yusuf','FrontEnd','Tavia'];
+let [firstName,skill,company] = ['Yusuf','FrontEnd','Tavia'];
 
-// console.log(firstName + ' is a '+skill );
+console.log(firstName + ' is a '+skill );
 
-//Object literals
-// function addressMaker(city,state){
-//   // const newAddress = {
-//   //   newCity:city,
-//   //   newState:state,
-//   // };
-//   const newAddress = {city,state};
-//   console.log(newAddress.city);
-// }
+Object literals
+function addressMaker(city,state){
+  // const newAddress = {
+  //   newCity:city,
+  //   newState:state,
+  // };
+  const newAddress = {city,state};
+  console.log(newAddress.city);
+}
 
 
-// addressMaker('Austin','Texas');
+addressMaker('Austin','Texas');
 
-// Object literals challenge
+Object literals challenge
 
-// function addressMaker(address){
-//   const {city,state} = address;
-//   const newAddress = {
-//     city,
-//     state,
-//     country:'United State'
-//   };
+function addressMaker(address){
+  const {city,state} = address;
+  const newAddress = {
+    city,
+    state,
+    country:'United State'
+  };
   
   
-//   console.log(`I am from ${city}, ${state} in the ${newAddress.country}`);
-// }
+  console.log(`I am from ${city}, ${state} in the ${newAddress.country}`);
+}
 
 
-// addressMaker({city:'Austin',state:'Texas'});
+addressMaker({city:'Austin',state:'Texas'});
 
-//For of loop used for any iterables.
+For of loop used for any iterables.
 
-// let incomes = [69000,70000,85000];
-// total = 0;
-// for(const income of incomes){
-//   total +=income;
-//   //console.log(income);
-// }
+let incomes = [69000,70000,85000];
+total = 0;
+for(const income of incomes){
+  total +=income;
+  //console.log(income);
+}
 
-// console.log(total);
+console.log(total);
 
-// let fullName = 'Yusuf, servant of Allah';
-// for (const char of fullName) {
-//     console.log(char);
-// }
+let fullName = 'Yusuf, servant of Allah';
+for (const char of fullName) {
+    console.log(char);
+}
 
-//challenge
-// for of is  only designed to iterate not to update iterables
+challenge
+for of is  only designed to iterate not to update iterables
 
-//spread operator
+spread operator
 
-// let example1 = [1,2,34,5,7,9]
-// let example2 = [...example1];
-// example2.push(true);
-// console.log(example1)
-// console.log(example2)
-// it works on objects too but not fully supported
+let example1 = [1,2,34,5,7,9]
+let example2 = [...example1];
+example2.push(true);
+console.log(example1)
+console.log(example2)
+it works on objects too but not fully supported
 
-// Rest operator
-// function add(...num){
-//   console.log(num)
-// }
+Rest operator
+function add(...num){
+  console.log(num)
+}
 
-// add(3)
-// add(3,4,5,5,2)
+add(3)
+add(3,4,5,5,2)
 
-//Arrow functions
+Arrow functions
 
-// function add(...num){
-//   // let total = num.reduce(function(x,y){
-//   //   return x+y;
-//   // });
+function add(...num){
+  // let total = num.reduce(function(x,y){
+  //   return x+y;
+  // });
 
-//   let total = num.reduce((x,y)=>x+y);
-//   console.log(total);
-// }
+  let total = num.reduce((x,y)=>x+y);
+  console.log(total);
+}
 
-// add(3,4,5,5,2);
+add(3,4,5,5,2);
 
-//Default Parameters
-// function add(numArray = []){
-//   let total= 0;
-//   numArray.forEach((element)=>{
-//     total += element;
-//   });
-//   console.log(total);
-// }
-// add();
+Default Parameters
+function add(numArray = []){
+  let total= 0;
+  numArray.forEach((element)=>{
+    total += element;
+  });
+  console.log(total);
+}
+add();
 
-// includes() instead of indexOf() not supported by ie
+includes() instead of indexOf() not supported by ie
 
-// let numArray = [2,3,4,6,7,8];
-// console.log(numArray.includes(0))// false
-// console.log(numArray.includes(6))// true
+let numArray = [2,3,4,6,7,8];
+console.log(numArray.includes(0))// false
+console.log(numArray.includes(6))// true
 
 // Let and Const
 
@@ -156,74 +156,74 @@ if(false){
 // console.log(example.padStart(10,'-'))
 // console.log(example.padEnd(10,'-'))
 
-// import { Animal } from "./animal.js";
+import { Animal } from "./animal.js";
 
-// let cat = new Animal('Fat',4);
+let cat = new Animal('Fat',4);
 
-//Trailing commas
-// function add(param){
-// const obj = {
-//   name:'Yusuf',
-// }
-// console.log(obj.name);
-// }
+Trailing commas
+function add(param){
+const obj = {
+  name:'Yusuf',
+}
+console.log(obj.name);
+}
 
-// add();
+add();
 
-//Async and Await
+Async and Await
 
-// const apiUrl = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
-// const apiUrl = 'https://api.github.com';
+const apiUrl = 'https://fcctop100.herokuapp.com/api/fccusers/top/alltime';
+const apiUrl = 'https://api.github.com';
 
-// function getTop100Campers(){
-//  fetch(apiUrl)
-//  .then((response)=>response.json())
-//  .then((json) =>{
-//    console.log(json[0]);
-//  }).catch((error)=>{
-//    console.log('Failed');
-//  });
-// // console.log(apiUrl);
+function getTop100Campers(){
+ fetch(apiUrl)
+ .then((response)=>response.json())
+ .then((json) =>{
+   console.log(json[0]);
+ }).catch((error)=>{
+   console.log('Failed');
+ });
+// console.log(apiUrl);
 
-// }
+}
 
-// async function getTop100Campers(){
-//   const response = await fetch(apiUrl);
-//   const json = await response.json();
-//   console.log(json[0]);
-// }
-// getTop100Campers();
+async function getTop100Campers(){
+  const response = await fetch(apiUrl);
+  const json = await response.json();
+  console.log(json[0]);
+}
+getTop100Campers();
 
-// function resolveAfter3Seconds(){
-//   return new Promise(resolve =>{
-//       setTimeout(()=>{
-//         resolve('resolved');
-//       },5000);
-//   });
-// }
+function resolveAfter3Seconds(){
+  return new Promise(resolve =>{
+      setTimeout(()=>{
+        resolve('resolved');
+      },5000);
+  });
+}
 
-// resolveAfter3Seconds().then((data)=>{
-//   console.log(data)
-// });
+resolveAfter3Seconds().then((data)=>{
+  console.log(data)
+});
 
-// async function getAsyncData(){
-//   const result = await resolveAfter3Seconds();
-//   console.log(result);
-// }
+async function getAsyncData(){
+  const result = await resolveAfter3Seconds();
+  console.log(result);
+}
 
 // getAsyncData();
 
-// Sets
-// const exampleSet  = new Set([1,1,1,1,1,2,2,2,2]);
-// exampleSet.add([2]);
-// exampleSet.add(8);
-// exampleSet.add(3).add(true);
-// // console.log(exampleSet.delete(3));// returns true
-// // console.log(exampleSet.has(5));// returns false
+Sets
+const exampleSet  = new Set([1,1,1,1,1,2,2,2,2]);
+exampleSet.add([2]);
+exampleSet.add(8);
+exampleSet.add(3).add(true);
+// console.log(exampleSet.delete(3));// returns true
+// console.log(exampleSet.has(5));// returns false
 
-// for (const iterator of exampleSet) {
-//     console.log(iterator);
-// }
+for (const iterator of exampleSet) {
+    console.log(iterator);
+}
 
-// console.log(exampleSet);
-// console.log(exampleSet.size);
+console.log(exampleSet);
+console.log(exampleSet.size);
